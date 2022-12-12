d64807e42d641c384a31c7c9eb9cc8d65a6ef726
mgrasshoff <martin.grasshoff@protonmail.com>
2022-12-12 18:57:38 +0100

    Add a better way to calculate the consensus information for maegatk input.

--- R/get_consensus.R
+++ R/get_consensus.R
@@ -11,9 +11,9 @@ get_consensus <- function(alt_base, ref_base, input_matrix, chromosome_prefix = 
   base_numeric <- c("A" = 8, "C" = 4, "G" = 2, "T" = 1)
   letter_numeric <- base_numeric[alt_base]
   ref_numeric <- base_numeric[ref_base]
-  mixture_numeric <- letter_numeric + ref_numeric
-  other_values <- 1:sum(base_numeric)
-  other_values <- other_values[!other_values %in% c(base_numeric[c(alt_base, ref_base)], mixture_numeric)]
+  #mixture_numeric <- letter_numeric + base_numeric
+  #other_values <- 1:sum(base_numeric)
+  #other_values <- other_values[!other_values %in% c(base_numeric[c(alt_base, ref_base)], mixture_numeric)]
   # It is possible, that a variant only has reads for a different alternative allele.
   # So, there are only C reads for the position 3107_N.
   # That means, that 3107_N>C has a value of 2 (Alt), while 3107_N>A would have a value of 3 (Both).
@@ -21,11 +21,25 @@ get_consensus <- function(alt_base, ref_base, input_matrix, chromosome_prefix = 
   other_homo_values <- base_numeric[!base_numeric %in% base_numeric[c(alt_base, ref_base)]]
 
   
-  output_matrix <- input_matrix
+  #output_matrix <- input_matrix
+  output_matrix <- matrix(0, nrow = nrow(input_matrix), ncol = ncol(input_matrix))
+  rownames(output_matrix) <- rownames(input_matrix)
+  colnames(output_matrix) <- colnames(input_matrix)
   output_matrix[input_matrix == ref_numeric] <- 1
   output_matrix[input_matrix == letter_numeric] <- 2
-  output_matrix[input_matrix == mixture_numeric] <- 3
-  output_matrix[input_matrix %in% other_values] <- 0
+
+  bit_and_test <- input_matrix
+  bit_and_test <- matrix(bitwAnd(letter_numeric, bit_and_test), nrow = nrow(input_matrix), ncol = ncol(input_matrix))
+  rownames(bit_and_test) <- rownames(input_matrix)
+  colnames(bit_and_test) <- colnames(bit_and_test)
+  bit_or_test <- input_matrix
+  bit_or_test <- matrix(bitwOr(letter_numeric, bit_or_test), nrow = nrow(input_matrix), ncol = ncol(input_matrix))
+  rownames(bit_or_test) <- rownames(input_matrix)
+  colnames(bit_or_test) <- colnames(bit_and_test)
+  output_matrix[(bit_and_test > 0) & (bit_or_test > letter_numeric)] <- 3
+
+  #output_matrix[input_matrix == mixture_numeric] <- 3
+  #output_matrix[input_matrix %in% other_values] <- 0
   output_matrix[input_matrix %in% other_homo_values] <- 0
   
   rownames(output_matrix) <- paste0(chromosome_prefix, "_", gsub("[^[:digit:]., ]", "", rownames(output_matrix)), "_", ref_base, "_", alt_base)
--- README.md
+++ README.md
@@ -32,7 +32,7 @@ The mutation data was obtained from the Sanger Institute Catalogue Of Somatic Mu
 
 ```
 
-# Current Features v0.2.1
+# Current Features v0.2.2
 
 - Loading data from VarTrix and MAEGATK.
 - Transforming the data to be compatible for joint analysis.
