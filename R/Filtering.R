ed7fce299132bad17fd8945907eb60099e1d6bfe
mgrasshoff <martin.grasshoff@protonmail.com>
2023-03-31 11:10:48 +0200

    Added alt reads thresholding for filtering.

--- R/Filtering.R
+++ R/Filtering.R
@@ -17,7 +17,8 @@
 #'@import fastmatch Matrix Seurat SummarizedExperiment
 #'@param se SummarizedExperiment object.
 #'@param blacklisted_barcodes_path Barcodes you want to remove. Path to a file with one column without header.
-#'@param fraction_threshold Variants with an VAF below this threshold are set to 0. Numeric.
+#'@param fraction_threshold Variants with an VAF below this threshold are set to 0. Numeric. Default = NULL.
+#'@param alts_threshold Variants with a number of alt reads less than this threshold are set to 0. Numeric. Default = NULL.
 #'@param min_cells_per_variant In how many cells should a variant be present to be included? Numeric. Default = 2.
 #'@param path_seurat Path to a Seurat object. Cells not present in the object will be removed.
 #'@param min_variants_per_cell How many variants should be covered in a cell have to be included? Default = 1.
@@ -28,7 +29,7 @@
 #'   se_geno <- Filtering(se_geno, min_cells_per_variant = 2, fraction_threshold = 0.05)
 #' }
 #'@export
-Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold = NULL, path_seurat = NULL, min_cells_per_variant = 2, min_variants_per_cell = 1){
+Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold = NULL, alts_threshold = NULL, path_seurat = NULL, min_cells_per_variant = 2, min_variants_per_cell = 1){
   if(!is.null(blacklisted_barcodes_path)){
     print("We remove the unwanted cell barcodes.")
     blacklisted_barcodes <- read.table(blacklisted_barcodes_path, header = FALSE)
@@ -75,6 +76,35 @@ Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold =
       assays(se)$fraction <- fraction_matrix
     }
   }
+
+
+  if(!is.null(alts_threshold)){
+    if(any(!is.numeric(alts_threshold), is.infinite(alts_threshold))){
+      stop("Your alts_threshold should be a numeric value.")
+    }
+    print("We assume that cells with a number of alternative reads smaller than our threshold are actually reference.")
+    print("We set consensus values to 1 (Ref), fraction values to 0 and alts to 0.")
+    # Filtering using sparse matrices.
+    consensus_matrix <- assays(se)$consensus
+    fraction_matrix <- assays(se)$fraction
+    coverage_matrix <- assays(se)$coverage
+    alts_matrix <- assays(se)$alts
+    position_matrix <- summary(alts_matrix)
+    position_matrix <- subset(position_matrix, x < alts_threshold)
+    # If no elements fall between 0 and the alts_threshold, we do not have to change the matrices.
+    if(nrow(position_matrix) > 0){
+      ij <- as.matrix(position_matrix[, 1:2])
+      consensus_matrix[ij] <- 1
+      fraction_matrix[ij] <- 0
+      coverage_matrix[ij] <- coverage_matrix[ij] - alts_matrix[ij]
+      alts_matrix[ij] <- 0
+      assays(se)$consensus <- consensus_matrix
+      assays(se)$fraction <- fraction_matrix
+      assays(se)$coverage <- coverage_matrix
+      assays(se)$alts <- alts_matrix
+    }
+  }
+
 
   print("We remove all the variants that are always NoCall.")
   consensus_test <- assays(se)$consensus > 0
