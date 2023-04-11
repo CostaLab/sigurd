d30a9130b70b684cf1de77525a43bee5f78cbc47
mgrasshoff <martin.grasshoff@protonmail.com>
2023-04-11 10:07:51 +0200

    Added option to Filtering.R to set cells below threshold to either NoCall or Reference.

--- R/Filtering.R
+++ R/Filtering.R
@@ -22,6 +22,7 @@
 #'@param min_cells_per_variant In how many cells should a variant be present to be included? Numeric. Default = 2.
 #'@param path_seurat Path to a Seurat object. Cells not present in the object will be removed.
 #'@param min_variants_per_cell How many variants should be covered in a cell have to be included? Default = 1.
+#'@param reject_value Should cells that fall below a threshold (fraction_threshold or alts_threshold) be treated as Reference or NoCall? Default = NoCall.
 #'@examples
 #' \dontrun{
 #'   # Removing all variants that are not detected in at least 2 cells.
@@ -29,7 +30,16 @@
 #'   se_geno <- Filtering(se_geno, min_cells_per_variant = 2, fraction_threshold = 0.05)
 #' }
 #'@export
-Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold = NULL, alts_threshold = NULL, path_seurat = NULL, min_cells_per_variant = 2, min_variants_per_cell = 1){
+Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold = NULL, alts_threshold = NULL, path_seurat = NULL, min_cells_per_variant = 2, min_variants_per_cell = 1, reject_value = "NoCall"){
+  # Checking if the reject_value variable is correct.
+  if(!reject_value %in% c("Reference", "NoCall")){
+    stop(paste0("Your reject_value is ", reject_value, ".\nIt should be Reference or NoCall."))
+  } else{
+    if(reject_value == "NoCall") reject_value_numeric <- 0
+    if(reject_value == "Reference") reject_value_numeric <- 1
+  }
+
+
   if(!is.null(blacklisted_barcodes_path)){
     print("We remove the unwanted cell barcodes.")
     blacklisted_barcodes <- read.table(blacklisted_barcodes_path, header = FALSE)
@@ -58,8 +68,8 @@ Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold =
     if(any(fraction_threshold >= 1, fraction_threshold <= 0)){
       stop("Your fraction threshold is not 0 < x < 1.")
     }
-    print("We assume that cells with a fraction smaller than our threshold are actually reference.")
-    print("We set consensus values to 1 (Ref) and fraction values to 0.")
+    print(paste0("We assume that cells with a fraction smaller than our threshold are actually ", reject_value, "."))
+    print(paste0("We set consensus values to ", reject_value_numeric, " (", reject_value, ") and fraction values to 0."))
     print(paste0("We do not set fractions between ", fraction_threshold, " and 1 to 1."))
     print("This way, we retain the heterozygous information.")
     # Filtering using sparse matrices.
@@ -70,7 +80,7 @@ Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold =
     # If no elements fall between 0 and the fraction_threshold, we do not have to change the matrices.
     if(nrow(position_matrix) > 0){
       ij <- as.matrix(position_matrix[, 1:2])
-      consensus_matrix[ij] <- 1
+      consensus_matrix[ij] <- reject_value_numeric
       fraction_matrix[ij] <- 0
       assays(se)$consensus <- consensus_matrix
       assays(se)$fraction <- fraction_matrix
@@ -82,26 +92,38 @@ Filtering <- function(se, blacklisted_barcodes_path = NULL, fraction_threshold =
     if(any(!is.numeric(alts_threshold), is.infinite(alts_threshold))){
       stop("Your alts_threshold should be a numeric value.")
     }
-    print("We assume that cells with a number of alternative reads smaller than our threshold are actually reference.")
-    print("We set consensus values to 1 (Ref), fraction values to 0 and alts to 0.")
+    print(paste0("We assume that cells with a number of alternative reads smaller than our threshold are actually ", reject_value, "."))
+    print(paste0("We set consensus values to ", reject_value_numeric, " (", reject_value, "), fraction values to 0."))
+    if(reject_value == "NoCall") print("We set Alts, Refs and Coverage to 0.")
+    if(reject_value == "Reference") print("We set Alts to 0 and adjust the Coverage.")
     # Filtering using sparse matrices.
     consensus_matrix <- assays(se)$consensus
     fraction_matrix <- assays(se)$fraction
     coverage_matrix <- assays(se)$coverage
     alts_matrix <- assays(se)$alts
+    refs_matrix <- assays(se)$refs
     position_matrix <- summary(alts_matrix)
     position_matrix <- subset(position_matrix, x < alts_threshold)
     # If no elements fall between 0 and the alts_threshold, we do not have to change the matrices.
     if(nrow(position_matrix) > 0){
       ij <- as.matrix(position_matrix[, 1:2])
-      consensus_matrix[ij] <- 1
+      consensus_matrix[ij] <- reject_value_numeric
       fraction_matrix[ij] <- 0
+      # Since we remove the Alt reads in either case (NoCall or Reference), we remove the Alts from the Coverage.
       coverage_matrix[ij] <- coverage_matrix[ij] - alts_matrix[ij]
+      # We now set the alts matrix to 0.
       alts_matrix[ij] <- 0
+      # If we set the cells to NoCall, we also set the Refs to 0.
+      # We then also set the coverage matrix to 0.
+      if(reject_value == "NoCall"){
+        refs_matrix[ij] <- 0
+	coverage_matrix[ij] <- 0
+      }
       assays(se)$consensus <- consensus_matrix
       assays(se)$fraction <- fraction_matrix
       assays(se)$coverage <- coverage_matrix
       assays(se)$alts <- alts_matrix
+      assays(se)$refs <- refs_matrix
     }
   }
 
