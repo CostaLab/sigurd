c0865efb733ef71a801628897e9263bb58a90668
mgrasshoff <martin.grasshoff@protonmail.com>
2023-06-12 10:50:36 +0200

    Add row data to merged object.

--- R/CombineSEobjects.R
+++ R/CombineSEobjects.R
@@ -21,6 +21,29 @@ CombineSEobjects <- function(se_somatic, se_MT, suffixes = c("_somatic", "_MT"))
   meta_data <- merge(meta_data_somatic, meta_data_MT, by = "Cell", all = TRUE, suffixes = suffixes)
   meta_data <- meta_data[match(cells, meta_data$Cell),]
 
+  meta_row_somatic <- rowData(se_somatic)
+  meta_row_MT      <- rowData(se_MT)
+  if(ncol(meta_row_somatic) > 0 & ncol(meta_row_MT) > 0){
+    meta_row <- merge(meta_row_somatic, meta_row_MT, by = "Cell", all = TRUE, suffixes = suffixes)
+    meta_row <- meta_row[match(cells, meta_row$Cell),]
+  } else if(ncol(meta_row_somatic) == 0 & ncol(meta_row_MT) > 0){
+    meta_row_somatic <- matrix(NA, nrow = nrow(meta_row_somatic), ncol = ncol(meta_row_MT))
+    rownames(meta_row_somatic) <- rownames(se_somatic)
+    colnames(meta_row_somatic) <- colnames(meta_row_MT)
+    meta_row_somatic <- DataFrame(meta_row_somatic)
+    meta_row_somatic$VariantName <- rownames(meta_row_somatic)
+    meta_row <- merge(meta_row_somatic, meta_row_MT, by = "VariantName", all = TRUE, suffixes = suffixes)
+    meta_row <- meta_row[match(features, meta_row$VariantName),]
+  } else if(ncol(meta_row_somatic) > 0 & ncol(meta_row_MT) > 0){
+    meta_row_MT <- matrix(NA, nrow = nrow(meta_row_MT), ncol = ncol(meta_row_somatic))
+    rownames(meta_row_MT) <- rownames(se_MT)
+    colnames(meta_row_MT) <- colnames(meta_row_somatic)
+    meta_row_MT <- DataFrame(meta_row_MT)
+    meta_row_MT$VariantName <- rownames(meta_row_MT)
+    meta_row <- merge(meta_row_somatic, meta_row_MT, by = "VariantName", all = TRUE, suffixes = suffixes)
+    meta_row <- meta_row[match(features, meta_row$VariantName),]
+  }
+
   #assays_somatic <- assays(se_somatic)
   #assays_somatic <- lapply(assays_somatic, as.matrix)
   #assays_MT <- assays(se_MT)
@@ -39,7 +62,7 @@ CombineSEobjects <- function(se_somatic, se_MT, suffixes = c("_somatic", "_MT"))
   names(assays_combined) <- assay_names_somatic
 
   se_combined <- SummarizedExperiment(assays = assays_combined,
-                                      colData = meta_data)
+                                      colData = meta_data, rowData = meta_row)
   return(se_combined)
 }
 
