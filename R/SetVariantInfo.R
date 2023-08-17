45d2211dc93e1a72d5ef67fe8808cf4b1a1e81b6
mgrasshoff <martin.grasshoff@protonmail.com>
2023-08-17 10:49:12 +0200

    Removed unnecessary packages.

--- R/SetVariantInfo.R
+++ R/SetVariantInfo.R
@@ -2,13 +2,13 @@
 #'@description
 #'We add the genotyping information for a set of variants to a Seurat object.
 #'The function returns a matrix with the values from the specified assay.
-#'@import SummarizedExperiment
+#'@import SummarizedExperiment Seurat
 #'@param SE SummarizedExperiment object.
-#'@param Seurat The Seurat object.
+#'@param seurat_object The Seurat object.
 #'@param information The assay with the desired information. Default: consensus
 #'@param variants A vector of variants.
 #'@export
-SetVariantInfo <- function(SE, Seurat, information = "consensus", variants = NULL){
+SetVariantInfo <- function(SE, seurat_object, information = "consensus", variants = NULL){
   # We check if a vector of variants has been supplied.
   if(is.null(variants)){
     stop("Error: You forgot to supply a vector of variants.")
@@ -24,7 +24,7 @@ SetVariantInfo <- function(SE, Seurat, information = "consensus", variants = NUL
     stop(paste0("Only ", variants_check, " of ", length(variants), " are in the SE object."))
   }
   # We check if the requested assay is actually present.
-  assay_check <- information %in% names(assays(SE))
+  assay_check <- information %in% names(SummarizedExperiment::assays(SE))
   if(!assay_check){
     stop("The assay you wants is not present in the object.")
   }
@@ -32,11 +32,11 @@ SetVariantInfo <- function(SE, Seurat, information = "consensus", variants = NUL
   # We check if all the cells are actually in the Seurat object.
   # If not, we only add the information for the ones present. 
   # We execute an error function if there are zero cells present.
-  cell_check_SE <- rownames(res) %in% colnames(Seurat)
+  cell_check_SE <- rownames(res) %in% colnames(seurat_object)
   if(sum(cell_check_SE) == 0){
     stop("Error: 0 cells from the SE object are in the Seurat object.")
   }
-  res <- res[rownames(res) %in% colnames(Seurat), , drop = FALSE]
-  Seurat <- AddMetaData(Seurat, res, col.name = colnames(res))
-  return(Seurat)
+  res <- res[rownames(res) %in% colnames(seurat_object), , drop = FALSE]
+  seurat_object <- Seurat::AddMetaData(seurat_object, res, col.name = colnames(res))
+  return(seurat_object)
 }
