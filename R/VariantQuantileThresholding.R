0482ca7630a3f3b310057a32ca42780871254ad7
mgrasshoff <martin.grasshoff@protonmail.com>
2024-03-08 14:21:24 +0100

    Added option to remove NoCall cells.

--- R/VariantQuantileThresholding.R
+++ R/VariantQuantileThresholding.R
@@ -22,13 +22,20 @@
 #'@param verbose Should the function be verbose? Default = TRUE
 #'@export
 VariantQuantileThresholding <- function(SE, min_coverage = 2, quantiles = c(0.1, 0.9), thresholds = c(0.1, 0.9), top_cells = NULL, top_VAF = NULL, min_quality = NULL, mean_allele_frequency = 0,
-                                        group_of_interest = NULL, group1 = NULL, group2 = NULL, group_factor = NULL, verbose = TRUE){
+                                        group_of_interest = NULL, group1 = NULL, group2 = NULL, group_factor = NULL, remove_nocall = TRUE, verbose = TRUE){
+  if(remove_nocall){
+    if(verbose) print("We set NoCall cells as NA.")
+    nocall_check <- SummarizedExperiment::assays(SE)[["consensus"]] == 0
+    SummarizedExperiment::assays(SE)[["fraction"]][nocall_check] <- NA
+    SummarizedExperiment::assays(SE)[["coverage"]][nocall_check] <- NA
+  }
+
   if(verbose) print("Get the mean allele frequency and coverage.")
   mean_af  <- Matrix::rowMeans(SummarizedExperiment::assays(SE)[["fraction"]], na.rm = TRUE)
   mean_cov <- Matrix::rowMeans(SummarizedExperiment::assays(SE)[["coverage"]], na.rm = TRUE)
   if(all(is.null(min_quality), is.numeric(min_quality))) stop("Error: Your minimum quality is not either NULL or a numeric.")
   if(all(is.null(mean_allele_frequency), is.numeric(mean_allele_frequency))) stop("Error: Your mean allele frequency is not either NULL or a numeric.")
-  
+
   if(all(!is.null(group_of_interest), !is.null(group1), !is.null(group2))){
     if(!group_of_interest %in% colnames(SummarizedExperiment::colData(SE))) stop("Error: Your group_of_interest is not in the colData.")
     if(!group1 %in% SummarizedExperiment::colData(SE)[, group_of_interest]) stop("Error: Your group1 is not in the group_of_interest.")
@@ -56,8 +63,7 @@ VariantQuantileThresholding <- function(SE, min_coverage = 2, quantiles = c(0.1,
     if(verbose) print("Thresholding using the quantile approach.")
     if(length(quantiles)  != 2) stop("Your quantiles are not of length 2.")
     if(length(thresholds) != 2) stop("Your thresholds are not of length 2.")
-    voi_ch <- subset(vars, vars$Mean_AF > mean_allele_frequency & vars$Mean_Cov > min_coverage & vars$Quantile1 < thresholds[1] & vars$Quantile2 > thresholds[2])
-
+    vois <- subset(vars, vars$Mean_AF > mean_allele_frequency & vars$Mean_Cov > min_coverage & vars$Quantile1 < thresholds[1] & vars$Quantile2 > thresholds[2])
 
   } else if(any(is.null(top_cells), is.null(top_VAF))){
     if(verbose) print("Get the quantiles of the VAFs of each variant.")
@@ -74,7 +80,7 @@ VariantQuantileThresholding <- function(SE, min_coverage = 2, quantiles = c(0.1,
     if(verbose) print("Thresholding using the quantile approach.")
     if(length(quantiles) != 2) stop("Your quantiles are not of length 2.")
     if(length(thresholds) != 2) stop("Your thresholds are not of length 2.")
-    voi_ch <- subset(vars, vars$Mean_AF > mean_allele_frequency & vars$Mean_Cov > min_coverage & vars$Quantile1 < thresholds[1] & vars$Quantile2 > thresholds[2])
+    vois <- subset(vars, vars$Mean_AF > mean_allele_frequency & vars$Mean_Cov > min_coverage & vars$Quantile1 < thresholds[1] & vars$Quantile2 > thresholds[2])
   } else{
     if(verbose) print("Get the quantile of the VAF of each variant.")
     if(length(quantiles) > 1) stop("You are providing more than 1 quantile. You should only provide 1.")
@@ -83,7 +89,7 @@ VariantQuantileThresholding <- function(SE, min_coverage = 2, quantiles = c(0.1,
     quantiles <- quantiles[[1]]
     top_cells_values <- SummarizedExperiment::assays(SE)[["fraction"]]
     top_cells_values <- top_cells_values > top_VAF
-    top_cells_values <- Matrix::rowSums(top_cells_values)
+    top_cells_values <- Matrix::rowSums(top_cells_values, na.rm = TRUE)
     if(!is.null(min_quality)){
       vars <- data.frame(Mean_AF = mean_af, Mean_Cov = mean_cov, Quality = SummarizedExperiment::rowData(SE)$VariantQuality, Quantile = quantiles, TopCells = top_cells_values)
       vars <- vars[!is.na(vars$VariantQuality), ]
@@ -91,8 +97,8 @@ VariantQuantileThresholding <- function(SE, min_coverage = 2, quantiles = c(0.1,
     } else{
       vars <- data.frame(Mean_AF = mean_af, Mean_Cov = mean_cov, Quantile = quantiles, TopCells = top_cells_values)
     }
-    voi_ch <- subset(vars, vars$Mean_Cov > min_coverage & vars$Quantile <= thresholds[1] & vars$TopCells >= top_cells)
+    vois <- subset(vars, vars$Mean_Cov > min_coverage & vars$Quantile <= thresholds[1] & vars$TopCells >= top_cells)
   }
-  voi_ch <- rownames(voi_ch)
-  return(voi_ch)
+  vois <- rownames(vois)
+  return(vois)
 }
