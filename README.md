fe242543e8082dbb49f01c45359e19bf86f71f15
mgrasshoff <martin.grasshoff@protonmail.com>
2024-03-20 19:55:48 +0100

    Updated the candidate variant acquisition script.

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ The mutation data was obtained from the Sanger Institute Catalogue Of Somatic Mu
 
 ```
 
-# Current Features v0.2.46
+# Current Features v0.2.47
 
 - Loading data from VarTrix and MAEGATK.
 - Transforming the data to be compatible for joint analysis.
--- preprocessing/CandidateVariantAcquisition/CandidateVariantAcquisition.sh
+++ preprocessing/CandidateVariantAcquisition/CandidateVariantAcquisition.sh
@@ -1,57 +1,84 @@
 #!/bin/bash
 
+echo "Supply a file with the genes, the COSMIC IDs, the regions and the histologies for which variants should be selected."
+echo "Simply set the value of a file is NO to skip the part."
 
-# Where are the input files?"
-INPUT_PATH="/Path/To/Cosmic/Public/Files/CosmicCodingMuts.vcf.gz"
+echo "Where are the input files?"
+echo "The following files from COSMIC are necessary:"
+echo "CosmicCodingMuts.vcf.gz"
+echo "CosmicGenomeScreensMutantExport.tsv, for the histology selection."
+COSMIC_CODING_MUTATIONS="/Path/To/Cosmic/Public/Files/Cosmic_GenomeScreensMutant_Normal_v99_GRCh38.vcf.gz"
+COSMIC_CANCER_MUTATION_CENSUS="/Path/To/Cosmic/Public/Files/cmc_export.tsv"
 
-# Where are the results supposed to go?
+echo "Where are the results supposed to go?"
 RESULT_PATH="/Path/To/Output/Folder/"
 
-# In how many samples should a variant have been detected?
-# This values is only used to subset variants that are from a gene or a genomic region.
-# Variants retrieved by their COSMIC ID are always returned.
+echo "In how many samples should a variant have been detected?"
+echo "This values is only used to subset variants that are from a gene or a genomic region."
+echo "Variants retrieved by their COSMIC ID are always returned."
 SAMPLE_COUNT=10
 
-# The list of genes of interest.
-# The GenesOfInterest.txt file should only contain one column with no header. The column should contain the gene names.
+echo "The list of genes of interest."
+echo "The GenesOfInterest.txt file should only contain one column with no header. The column should contain the gene names."
 PATH_TO_GENES_OF_INTEREST="Example_Genes_Of_Interest.tsv"
 
-# COSMIC Variants of interest.
+echo "COSMIC IDs for candidate variants."
 PATH_TO_COSMIC_IDS="Example_COSMIC_IDs.tsv"
 
-# The regions of interest.
+echo "The regions of interest."
 PATH_TO_REGIONS="Example_Regions_Of_Interest.tsv"
 
+echo "The histologies of interest."
+PATH_TO_HISTOLOGIES="Example_Histologies_Of_Interest.tsv"
+
 echo "The used variables:"
-echo "The input files:     ${INPUT_PATH}"
-echo "The output folder:   ${RESULT_PATH}"
-echo "The candidate genes: ${PATH_TO_GENES_OF_INTEREST}"
-echo "The COSMIC IDs:      ${PATH_TO_COSMIC_IDS}"
-echo "The regions:         ${PATH_TO_REGIONS}"
+echo "The coding mutations: ${COSMIC_CODING_MUTATIONS}"
+echo "The CMC:              ${COSMIC_CANCER_MUTATION_CENSUS}"
+echo "The output folder:    ${RESULT_PATH}"
+echo "The sample count:     ${SAMPE_COUNT}"
+echo "The candidate genes:  ${PATH_TO_GENES_OF_INTEREST}"
+echo "The COSMIC IDs:       ${PATH_TO_COSMIC_IDS}"
+echo "The regions:          ${PATH_TO_REGIONS}"
+echo "The histologies:      ${PATH_TO_HISTOLOGIES}"
 
 
 
 
-echo "We get the header of the COSMIC file."
-tabix -p vcf ${INPUT_PATH} # We index the vcf.gz file. -p means that we set the input format as vcf.
-tabix -H ${INPUT_PATH} > ${RESULT_PATH}/CosmicSubset_Intermediary.vcf # -H only get the header.
+echo "We index the VCF file."
+tabix -p vcf ${COSMIC_CODING_MUTATIONS} # We index the vcf.gz file. -p means that we set the input format as vcf.
+bcftools view -h ${COSMIC_CODING_MUTATIONS} > ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
 
-echo "We get variants for the genes of interest."
-readarray -t genes_array < ${PATH_TO_GENES_OF_INTEREST}
-for gene in ${genes_array[*]}; do
-    echo ${gene}
-    bcftools view -H -i "CNT>=${SAMPLE_COUNT} && GENE='${gene}'" ${INPUT_PATH} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
-done
+if [ "${PATH_TO_GENES_OF_INTEREST}" != "NO" ]; then
+  echo "We get variants for the genes of interest."
+  readarray -t genes_array < ${PATH_TO_GENES_OF_INTEREST}
+  for gene in ${genes_array[*]}; do
+      echo ${gene}
+      bcftools view -H -i "SAMPLE_COUNT>=${SAMPLE_COUNT} && GENE='${gene}'" ${COSMIC_CODING_MUTATIONS} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
+  done
+fi
 
-echo "We get variants by their COSMIC IDs."
-readarray -t cosmic_array < ${PATH_TO_COSMIC_IDS}
-for cosmic in ${cosmic_array[*]}; do
-    echo ${cosmic}
-    bcftools view -H -i "ID='${cosmic}'" ${INPUT_PATH} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
-done
+if [ "${PATH_TO_COSMIC_IDS}" != "NO" ]; then
+  echo "We get variants by their COSMIC IDs."
+  readarray -t cosmic_array < ${PATH_TO_COSMIC_IDS}
+  for cosmic in ${cosmic_array[*]}; do
+      echo ${cosmic}
+      bcftools view -H -i "ID='${cosmic}'" ${COSMIC_CODING_MUTATIONS} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
+  done
+fi
 
-echo "We get variants in specified regions."
-bcftools view -H -R ${PATH_TO_REGIONS} ${INPUT_PATH} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
+if [ "${PATH_TO_REGIONS}" != "NO" ]; then
+  echo "We get variants in specified regions."
+  bcftools view -H -R ${PATH_TO_REGIONS} ${COSMIC_CODING_MUTATIONS} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
+fi
+
+if [ "${PATH_TO_HISTOLOGIES}" != "NO" ]; then
+  echo "We get variants for specified histologies."
+  readarray -t histologies_array < ${PATH_TO_HISTOLOGIES}
+  for histology in ${histologies_array[*]}; do
+      echo ${histology}
+      grep ${histology} ${COSMIC_CANCER_MUTATION_CENSUS} | awk -F '\t' '{print $19}' | xargs -I{} grep {} ${COSMIC_CODING_MUTATIONS} >> ${RESULT_PATH}/CosmicSubset_Intermediary.vcf
+  done
+fi
 
 echo "We remove any duplicates."
 bcftools norm --rm-dup all ${RESULT_PATH}/CosmicSubset_Intermediary.vcf > ${RESULT_PATH}/CosmicSubset.vcf
